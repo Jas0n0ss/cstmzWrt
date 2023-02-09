312d4dc5ddab43b3f5c0883ff3cf3f3daad96d7f
Jas0n0ss <88020021+Jas0n0ss@users.noreply.github.com>
2023-02-09 22:24:15 +0800

    Update network-plugin.sh

--- script/network-plugin.sh
+++ script/network-plugin.sh
@@ -6,7 +6,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
+# echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
 # download theme
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
