6ed008c7324cf5eb830a9a9ce35281508d7d784e
Jas0n0ss <88020021+Jas0n0ss@users.noreply.github.com>
2023-02-14 00:50:30 +0800

    Update network-plugin.sh

--- script/network-plugin.sh
+++ script/network-plugin.sh
@@ -8,9 +8,13 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >> /workdir/openwr
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> /workdir/openwrt/feeds.conf.default
 # echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
 # download theme
-#rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
+rm -rf package/lean/luci-theme-argon
+# for openwrt offical
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git /workdir/openwrt/package/luci-theme-argon
+# for lede 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git /workdir/openwrt/package/luci-theme-argon
+####
+git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git /workdir/openwrt/package/luci-theme-edge
 
 # 
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
