5c963f2ed2efad29f7e3b59c6dbb79ded76d1c32
Jas0n0ss <88020021+Jas0n0ss@users.noreply.github.com>
2023-02-14 18:57:37 +0800

    Update default-ip.sh

--- script/default-ip.sh
+++ script/default-ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set default manage ip addr
-sed -i 's/192.168.1.1/192.168.10.1/g' /workdir/openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' /workdir/openwrt/package/base-files/files/bin/config_generate
 
 # set default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' /workdir/openwrt/feeds/luci/collections/luci/Makefile
