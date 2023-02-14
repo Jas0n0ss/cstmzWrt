b1f2f7960a32f319737e73417cb6fe1418e80d52
Jas0n0ss <88020021+Jas0n0ss@users.noreply.github.com>
2023-02-14 22:14:19 +0800

    Update add-network-plugin.sh

--- scripts/add-network-plugin.sh
+++ scripts/add-network-plugin.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # add network plugin
+FEEDS_FILE=feeds.conf.default
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
-echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
+cat >>${FEEDS_FILE}<<EOF
+src-git helloworld https://github.com/fw876/helloworld
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages
+src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci
+src-git vssr https://github.com/jerrykuku/luci-app-vssr
+EOF
